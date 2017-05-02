82d23b0d76155779a0ecd111b024d0adcaa15515
Kukulkan94 <sergio.diazcruz@gmail.com>
2017-05-01 23:24:29 -0500

    chequen los efectos de sonido

--- app/src/main/java/nougat/team/team_nougat_app/MainActivity.java
+++ app/src/main/java/nougat/team/team_nougat_app/MainActivity.java
@@ -2,6 +2,8 @@ package nougat.team.team_nougat_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,23 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer mediaPlayer;
+        mediaPlayer = MediaPlayer.create(this,R.raw.fondo);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(100,100);
+        mediaPlayer.start();
     }
+
+
+    public void puntaje(View v) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.bm);
+        mp.start();
+    }
+
+    public void jugar(View v) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.bm);
+        mp.start();
+    }
+
 }
