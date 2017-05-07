1541b95852022f2374766a434b0713540cbbdd1f
HugoAlber0 <hugorivera.cool.player@gmail.com>
2017-05-07 14:10:03 -0500

    Se implemento el metodo generar(); para realizar el cambio de banderas en la vista

--- app/src/main/java/nougat/team/team_nougat_app/Juego.java
+++ app/src/main/java/nougat/team/team_nougat_app/Juego.java
@@ -2,16 +2,20 @@ package nougat.team.team_nougat_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Random;
 
-public class Juego extends AppCompatActivity {
+public class Juego extends AppCompatActivity
+{
     private ImageView vista_bandera;
     private BanderaAleatoria bandera_azar;
     private String nombre_bandera ="";
     int ruta_bandera =0;
+    Button refrescar;
 
 
     @Override
@@ -20,11 +24,24 @@ public class Juego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
         vista_bandera = (ImageView) findViewById(R.id.iv_Bandera);
+        refrescar=(Button)findViewById(R.id.bRefrescar) ;
+        refrescar.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                generar();
+            }
+        });
+        generar();
+    }
+
+    private void generar ()
+    {
         bandera_azar = new BanderaAleatoria();
         bandera_azar.obtenerBanderaAleatoria();
         nombre_bandera = bandera_azar.getNombre();
         ruta_bandera = bandera_azar.getRuta();
-        Toast.makeText(this, nombre_bandera + " - " + ruta_bandera, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, nombre_bandera + " - " + ruta_bandera, Toast.LENGTH_SHORT).show();
         vista_bandera.setImageResource(ruta_bandera);
     }
 
